831a71069cf08dc10b31645219a1f26055dd2efa
seandizon <43098042+seandizon@users.noreply.github.com>
2021-03-13 17:05:47 -0800

    Set up Login for Authentication
    Set up basic login for users and token retrieval. Still need to error handle people trying to skip directly to the webplayer.
    
    No logout as of now and still need to figure out some way to handle token storage per user.

--- modivibe/modivibe/webplayer/SpotifyApiObjs.py
+++ modivibe/modivibe/webplayer/SpotifyApiObjs.py
@@ -1,12 +1,36 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from modivibe.modivibe.settings import env
+from spotipy.oauth2 import SpotifyOAuth, CacheFileHandler
+from modivibe.settings import env
 
 # DON'T CHANGE ANY OF THE VARIABLES IN HERE pls
+
+# May need to write our own cache_handler, cache handled by spotipy defaults right now
+
+scope = [
+    'ugc-image-upload',
+    'user-read-recently-played',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'app-remote-control',
+    'streaming',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-follow-modify',
+    'user-follow-read',
+    'user-library-modify',
+    'user-library-read',
+    'user-read-email',
+    'user-read-private'
+]
 
 auth_manager = SpotifyOAuth(client_id=env('CLIENT_ID'),
                             client_secret=env('CLIENT_SECRET'),
                             redirect_uri=env('REDIRECT_URI'),
-                            scope='user-library-read')
+                            scope=' '.join(scope))
 
 sp = spotipy.Spotify(auth_manager=auth_manager)
--- modivibe/modivibe/webplayer/urls.py
+++ modivibe/modivibe/webplayer/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.splash, name='splash'),
+    path('redirectToHome', views.redirectToHome, name='redirectToHome'),
     path('webplayer', views.home, name='webplayer'),
     path('webplayer/settings', views.settings, name='webplayer/settings')
 ]
