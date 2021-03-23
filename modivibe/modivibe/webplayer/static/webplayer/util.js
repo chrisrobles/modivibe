e00f50af6081b74abfb64af4211556c717e36cd9
Ryan Sands <sandsr1@unlv.nevada.edu>
2021-03-23 15:38:10 -0700

    more css styling, clean up

--- modivibe/modivibe/webplayer/static/webplayer/util.js
+++ modivibe/modivibe/webplayer/static/webplayer/util.js
@@ -1,8 +1,9 @@
 // Utility functions for general use in the app
+
 // Converts milliseconds to a MM:SS format
-function millisToMinutesAndSeconds(millis) {
-  let minutes = Math.floor(millis / 60000);
-  let seconds = ((millis % 60000) / 1000).toFixed(0);
+function millisToMinutesAndSeconds(milliseconds) {
+  let minutes = Math.floor(milliseconds / 60000);
+  let seconds = ((milliseconds % 60000) / 1000).toFixed(0);
 
   return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
