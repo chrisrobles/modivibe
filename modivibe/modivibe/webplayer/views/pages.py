f9db49d7e5d7cdae95afc0f18e827ecc208246d3
Ryan Sands <sandsr1@unlv.nevada.edu>
2021-05-03 21:03:29 -0700

    clean up and fixed a bug preventing the liked songs pg html from being shown
    Still need to fix the createSongList() 3rd parameter for the uri in views.likedSongs() - see how we're overcoming that already in views.search()

--- modivibe/modivibe/webplayer/views/pages.py
+++ modivibe/modivibe/webplayer/views/pages.py
@@ -152,8 +152,6 @@ def playlist(request, playlist_id):
         return redirect('splash')
 
     if isAjaxRequest(request):
-        print('views.playlist() - isAjaxRequest(request) = TRUE')
-
         startAt = 0
         lim = 100
         pNo = 1
@@ -161,7 +159,7 @@ def playlist(request, playlist_id):
         slInfo = sp.playlist_items(playlist_id=playlist_id, limit=lim, offset=startAt)
         numSongs = slInfo['total']  # total number of songs in a playlist
 
-        print(dumps(slInfo, indent=4))
+        # print(dumps(slInfo, indent=4))
 
         info = []
 
@@ -203,7 +201,7 @@ def playlist(request, playlist_id):
 
 def likedSongs(request):
     print('****** views.likedSongs() fired ******')
-    print(isAjaxRequest(request))
+    # print(isAjaxRequest(request))
 
     if not validUser(request):
         return JsonResponse({'status': 401}) if isAjaxRequest(request) else redirect('splash')
@@ -212,79 +210,13 @@ def likedSongs(request):
     if not userAccessCode:
         return redirect('splash')
 
-    # if isAjaxRequest(request):
-    #     print('views.likedSongs() - isAjaxRequest(request) = TRUE')
-    #
-    #     # startAt = 0
-    #     # lim = 100
-    #     songNum = 1
-    #
-    #     # no params setting yet - uses defaults
-    #     likedSongsInfo = sp.current_user_saved_tracks()
-    #     numSongs = likedSongsInfo['total']  # total number of liked songs
-    #
-    #     # print(dumps(likedSongsInfo, indent=4))
-    #     print(numSongs)
-    #
-    #     info = []
-    #
-    #     for s in likedSongsInfo['items']:
-    #         info.append({
-    #             "songNum": songNum,
-    #             "songName": s['track']['name'],
-    #             "songId": s['track']['id'],
-    #             "songURI": s['track']['uri'],
-    #             "songArtist": s['track']['artists'][0]['name'] if s['track'].get('artists') else "",
-    #             "artistId": s['track']['artists'][0]['id'] if s['track'].get('artists') else "",
-    #             "songLength": s['track']['duration_ms']
-    #         })
-    #
-    #         songNum += 1
-    #
-    #     # commented out lim and startAt for now
-    #
-    #     # while lim + startAt < numSongs:
-    #     #     startAt += lim
-    #     #
-    #     #     likedSongsInfo = sp.current_user_saved_tracks() # no params setting yet - uses defaults
-    #     #     for s in likedSongsInfo['items']:
-    #     #         info.append({
-    #     #             "songNum": songNum,
-    #     #             "songName": s['track']['name'],
-    #     #             "songId": s['track']['id'],
-    #     #             "songURI": s['track']['uri'],
-    #     #             "songArtist": s['track']['artists'][0]['name'] if s['track'].get('artists') else "",
-    #     #             "artistId": s['track']['artists'][0]['id'] if s['track'].get('artists') else "",
-    #     #             "songLength": s['track']['duration_ms']
-    #     #         })
-    #     #
-    #     #         songNum += 1
-    #
-    #     # TODO: Make the songList an actual HTML page then utilize context & render_to_string
-    #     # page = createSongList(info, 'playlist', 'spotify:playlist:' + playlist_id)
-    #
-    #     print(dumps(info, indent=4))
-    #
-    #
-    #     # change "page" to the json object when done
-    #     return JsonResponse({'page': "page", 'status': 200})
-
-    # skipping the ajax stuff for now
-    # print('views.likedSongs() - isAjaxRequest(request) = TRUE')
-
-    # startAt = 0
-    # lim = 100
-
     startAt = 0
-    # might want to play with this limit .... a limit of 100 was giving an error
+    # might want to play with this limit .... a limit of 100 was giving an error - max of 50 apparently
     lim = 25
     songNum = 1
 
     likedSongsInfo = sp.current_user_saved_tracks(limit=lim, offset=startAt)
     numOfSongs = likedSongsInfo['total']  # total number of liked songs
-
-    # print(dumps(likedSongsInfo, indent=4))
-    print('numOfSongs: ', numOfSongs)
 
     info = []
 
@@ -301,7 +233,6 @@ def likedSongs(request):
 
         songNum += 1
 
-    # commented out lim and startAt for now
     while lim + startAt < numOfSongs:
         startAt += lim
 
@@ -320,12 +251,10 @@ def likedSongs(request):
             songNum += 1
 
     # TODO: Make the songList an actual HTML page then utilize context & render_to_string
-    # page = createSongList(info, 'liked songs', 'spotify:playlist:' + playlist_id)
+    page = createSongList(info, 'liked songs', 'likedSongs')
+    # page = createSongList(info, 'liked songs', 'spotify:likedSongs')
 
-    print(dumps(info, indent=4))
-
-    # change "page" to the json object when done
-    return JsonResponse({'page': "page", 'status': 200})
+    return JsonResponse({'page': page, 'status': 200})
 
     # return redirect('webplayer')  # fix this <------- need html page
 
@@ -534,8 +463,12 @@ def artistTopSongs(request, artist_id):
     content = createSongList(info, 'artist', 'spotify:artist:' + artist_id)
 
     if isAjaxRequest(request):
+        print('***** views.artistTopSongs() fired , is an ajax request *****')
+
         return JsonResponse({"content": content, 'status': 200})
     else:
+        print('***** views.artistTopSongs() fired , is NOT an ajax request *****')
+
         # if not ajax, have to get header info and insert content string into template
         header = getArtistHeaderInfo(sp, artist_id)
         return render(request, 'webplayer/artistPage.html',
