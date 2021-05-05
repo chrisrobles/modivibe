89bf913ff82335fc645ee44c39e395f617349306
chrisPROD <christian.robles@aol.com>
2021-05-05 19:17:40 +0000

    changed secret key to environment variables

--- modivibe/modivibe/modivibe/settings.py
+++ modivibe/modivibe/modivibe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(a^y5jn&cbif%a1v*%b6t66!g7qbwbryyv+d)@s3kz&7uwk7hm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
